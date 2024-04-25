ea6b2610ce2ba837f21ff8cf20185e4fe9c242b0
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2024-04-25 10:16:13 +0200

    Fix Terraform client initialization in tests

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -112,9 +112,6 @@ var _ = BeforeSuite(func() {
 		Fail("Environment variable TFC_TOKEN is required, but either not set or empty")
 	}
 	// Terraform Cloud Client
-	tfClient, err = tfc.NewClient(&tfc.Config{Token: os.Getenv("TFC_TOKEN")})
-	Expect(err).ToNot(HaveOccurred())
-	Expect(tfClient).ToNot(BeNil())
 	httpClient := tfc.DefaultConfig().HTTPClient
 	insecure := false
 	if v, ok := os.LookupEnv("TFC_TLS_SKIP_VERIFY"); ok {
