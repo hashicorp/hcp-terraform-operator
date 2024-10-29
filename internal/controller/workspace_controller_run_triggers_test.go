b01496320dff721f6e8abee4c2db0a3e2589f41e
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2024-10-29 17:12:02 +0100

    Migrate to go/v4 plugin (#498)

---
+++ internal/controller/workspace_controller_run_triggers_test.go
@@ -0,0 +1,145 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package controller
+
+import (
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	tfc "github.com/hashicorp/go-tfe"
+	appv1alpha2 "github.com/hashicorp/hcp-terraform-operator/api/v1alpha2"
+)
+
+var _ = Describe("Workspace controller", Ordered, func() {
+	var (
+		instance       *appv1alpha2.Workspace
+		namespacedName types.NamespacedName
+		workspace      string
+
+		wsName  string
+		wsName2 string
+		wsID    string
+		wsID2   string
+	)
+
+	BeforeAll(func() {
+		// Set default Eventually timers
+		SetDefaultEventuallyTimeout(syncPeriod * 4)
+		SetDefaultEventuallyPollingInterval(2 * time.Second)
+	})
+
+	BeforeEach(func() {
+		namespacedName = newNamespacedName()
+		workspace = fmt.Sprintf("kubernetes-operator-%v", randomNumber())
+		wsName = fmt.Sprintf("kubernetes-operator-source-%v", randomNumber())
+		wsName2 = fmt.Sprintf("kubernetes-operator-source-2-%v", randomNumber())
+		wsID = createWorkspaceForTests(wsName)
+		wsID2 = createWorkspaceForTests(wsName2)
+		// Create a new workspace object for each test
+		instance = &appv1alpha2.Workspace{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "app.terraform.io/v1alpha2",
+				Kind:       "Workspace",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              namespacedName.Name,
+				Namespace:         namespacedName.Namespace,
+				DeletionTimestamp: nil,
+				Finalizers:        []string{},
+			},
+			Spec: appv1alpha2.WorkspaceSpec{
+				Organization: organization,
+				Token: appv1alpha2.Token{
+					SecretKeyRef: &corev1.SecretKeySelector{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: secretNamespacedName.Name,
+						},
+						Key: secretKey,
+					},
+				},
+				Name: workspace,
+			},
+			Status: appv1alpha2.WorkspaceStatus{},
+		}
+	})
+
+	AfterEach(func() {
+		deleteWorkspace(instance)
+		Expect(tfClient.Workspaces.DeleteByID(ctx, wsID)).Should(Succeed())
+		Expect(tfClient.Workspaces.DeleteByID(ctx, wsID2)).Should(Succeed())
+	})
+
+	Context("Run Triggers", func() {
+		It("can handle run triggers by name", func() {
+			instance.Spec.RunTriggers = []appv1alpha2.RunTrigger{
+				{Name: wsName},
+				{Name: wsName2},
+			}
+			// Create a new Kubernetes workspace object and wait until the controller finishes the reconciliation
+			createWorkspace(instance)
+
+			Eventually(func() bool {
+				rt, err := tfClient.RunTriggers.List(ctx, instance.Status.WorkspaceID, &tfc.RunTriggerListOptions{
+					RunTriggerType: tfc.RunTriggerInbound,
+				})
+				Expect(rt).ShouldNot(BeNil())
+				Expect(err).Should(Succeed())
+				return hasWorkspaceSource(wsID, rt) && hasWorkspaceSource(wsID2, rt)
+			}).Should(BeTrue())
+		})
+
+		It("can handle run triggers by ID", func() {
+			instance.Spec.RunTriggers = []appv1alpha2.RunTrigger{
+				{ID: wsID},
+				{ID: wsID2},
+			}
+			// Create a new Kubernetes workspace object and wait until the controller finishes the reconciliation
+			createWorkspace(instance)
+
+			Eventually(func() bool {
+				rt, err := tfClient.RunTriggers.List(ctx, instance.Status.WorkspaceID, &tfc.RunTriggerListOptions{
+					RunTriggerType: tfc.RunTriggerInbound,
+				})
+				Expect(rt).ShouldNot(BeNil())
+				Expect(err).Should(Succeed())
+				return hasWorkspaceSource(wsID, rt) && hasWorkspaceSource(wsID2, rt)
+			}).Should(BeTrue())
+		})
+
+		It("can handle run triggers by mix of Name and ID", func() {
+			instance.Spec.RunTriggers = []appv1alpha2.RunTrigger{
+				{ID: wsID},
+				{Name: wsName2},
+			}
+			// Create a new Kubernetes workspace object and wait until the controller finishes the reconciliation
+			createWorkspace(instance)
+
+			Eventually(func() bool {
+				rt, err := tfClient.RunTriggers.List(ctx, instance.Status.WorkspaceID, &tfc.RunTriggerListOptions{
+					RunTriggerType: tfc.RunTriggerInbound,
+				})
+				Expect(rt).ShouldNot(BeNil())
+				Expect(err).Should(Succeed())
+				return hasWorkspaceSource(wsID, rt) && hasWorkspaceSource(wsID2, rt)
+			}).Should(BeTrue())
+		})
+	})
+})
+
+func hasWorkspaceSource(workspaceID string, runTriggerList *tfc.RunTriggerList) bool {
+	for _, v := range runTriggerList.Items {
+		if workspaceID == v.Sourceable.ID {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ internal/controller/workspace_controller_team_access.go
@@ -0,0 +1,292 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	tfc "github.com/hashicorp/go-tfe"
+	appv1alpha2 "github.com/hashicorp/hcp-terraform-operator/api/v1alpha2"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func getTeamID(teams map[string]*tfc.Team, instanceTeam appv1alpha2.Team) (string, error) {
+	if instanceTeam.Name != "" {
+		if t, ok := teams[instanceTeam.Name]; ok {
+			return t.ID, nil
+		}
+		return "", fmt.Errorf("team ID was not found by name %q", instanceTeam.Name)
+	}
+
+	if instanceTeam.ID != "" {
+		for _, v := range teams {
+			if v.ID == instanceTeam.ID {
+				return v.ID, nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("team ID was not found by ID %q", instanceTeam.ID)
+}
+
+func (r *WorkspaceReconciler) getInstanceTeamAccess(ctx context.Context, w *workspaceInstance) (map[string]*tfc.TeamAccess, error) {
+	o := map[string]*tfc.TeamAccess{}
+
+	if w.instance.Spec.TeamAccess == nil {
+		return o, nil
+	}
+
+	teams, err := r.getTeams(ctx, w)
+	if err != nil {
+		return o, err
+	}
+
+	for _, ta := range w.instance.Spec.TeamAccess {
+		tID, err := getTeamID(teams, ta.Team)
+		if err != nil {
+			w.log.Error(err, "Reconcile Team Access", "msg", "failed to get team ID")
+			r.Recorder.Event(&w.instance, corev1.EventTypeWarning, "ReconcileTeamAccess", "Failed to get team ID")
+			return o, err
+		}
+
+		o[tID] = &tfc.TeamAccess{
+			Team: &tfc.Team{
+				ID: tID,
+			},
+			Workspace: &tfc.Workspace{
+				ID: w.instance.Status.WorkspaceID,
+			},
+			Access:           tfc.AccessType(ta.Access),
+			Runs:             tfc.RunsPermissionType(ta.Custom.Runs),
+			RunTasks:         ta.Custom.RunTasks,
+			SentinelMocks:    tfc.SentinelMocksPermissionType(ta.Custom.Sentinel),
+			StateVersions:    tfc.StateVersionsPermissionType(ta.Custom.StateVersions),
+			Variables:        tfc.VariablesPermissionType(ta.Custom.Variables),
+			WorkspaceLocking: ta.Custom.WorkspaceLocking,
+		}
+	}
+
+	return o, nil
+}
+
+func (r *WorkspaceReconciler) getWorkspaceTeamAccess(ctx context.Context, w *workspaceInstance) (map[string]*tfc.TeamAccess, error) {
+	o := map[string]*tfc.TeamAccess{}
+
+	listOpts := &tfc.TeamAccessListOptions{
+		WorkspaceID: w.instance.Status.WorkspaceID,
+		ListOptions: tfc.ListOptions{
+			PageSize: maxPageSize,
+		},
+	}
+	for {
+		t, err := w.tfClient.Client.TeamAccess.List(ctx, listOpts)
+		if err != nil {
+			return o, err
+		}
+		for _, ta := range t.Items {
+			o[ta.Team.ID] = ta
+		}
+		if t.NextPage == 0 {
+			break
+		}
+		listOpts.PageNumber = t.NextPage
+	}
+	return o, nil
+}
+
+func (r *WorkspaceReconciler) getTeams(ctx context.Context, w *workspaceInstance) (map[string]*tfc.Team, error) {
+	teams := make(map[string]*tfc.Team)
+
+	fTeams := []string{}
+	for _, t := range w.instance.Spec.TeamAccess {
+		if t.Team.Name != "" {
+			fTeams = append(fTeams, t.Team.Name)
+		}
+	}
+
+	listOpts := &tfc.TeamListOptions{
+		Names: fTeams,
+		ListOptions: tfc.ListOptions{
+			PageSize: maxPageSize,
+		},
+	}
+	for {
+		tl, err := w.tfClient.Client.Teams.List(ctx, w.instance.Spec.Organization, listOpts)
+		if err != nil {
+			return teams, err
+		}
+		for _, t := range tl.Items {
+			teams[t.Name] = t
+		}
+		if tl.NextPage == 0 {
+			break
+		}
+		listOpts.PageNumber = tl.NextPage
+	}
+
+	return teams, nil
+}
+
+func teamAccessDifference(a, b map[string]*tfc.TeamAccess) map[string]*tfc.TeamAccess {
+	d := make(map[string]*tfc.TeamAccess)
+
+	for k, v := range a {
+		if _, ok := b[k]; !ok {
+			d[k] = v
+		}
+	}
+
+	return d
+}
+
+func getTeamAccessToCreate(specTeamAccess, workspaceTeamAccess map[string]*tfc.TeamAccess) map[string]*tfc.TeamAccess {
+	return teamAccessDifference(specTeamAccess, workspaceTeamAccess)
+}
+
+func getTeamAccessToDelete(specTeamAccess, workspaceTeamAccess map[string]*tfc.TeamAccess) map[string]*tfc.TeamAccess {
+	return teamAccessDifference(workspaceTeamAccess, specTeamAccess)
+}
+
+func getTeamAccessToUpdate(specTeamAccess, workspaceTeamAccess map[string]*tfc.TeamAccess) map[string]*tfc.TeamAccess {
+	ta := make(map[string]*tfc.TeamAccess)
+
+	if len(specTeamAccess) == 0 || len(workspaceTeamAccess) == 0 {
+		return ta
+	}
+
+	for ik, iv := range specTeamAccess {
+		if wv, ok := workspaceTeamAccess[ik]; ok {
+			iv.ID = wv.ID
+			if iv.Access == tfc.AccessCustom {
+				if !cmp.Equal(iv, wv, cmpopts.IgnoreFields(tfc.TeamAccess{}, "ID", "Team", "Workspace")) {
+					ta[ik] = iv
+				}
+			} else {
+				if iv.Access != wv.Access {
+					ta[ik] = iv
+				}
+			}
+		}
+	}
+
+	return ta
+}
+
+func (r *WorkspaceReconciler) createTeamAccess(ctx context.Context, w *workspaceInstance, createTeamAccess map[string]*tfc.TeamAccess) error {
+	workspaceID := w.instance.Status.WorkspaceID
+	for tID, v := range createTeamAccess {
+		option := tfc.TeamAccessAddOptions{
+			Workspace: &tfc.Workspace{
+				ID: workspaceID,
+			},
+			Team: &tfc.Team{
+				ID: tID,
+			},
+			Access: &v.Access,
+		}
+
+		if v.Access == tfc.AccessCustom {
+			option.Runs = &v.Runs
+			option.RunTasks = &v.RunTasks
+			option.SentinelMocks = &v.SentinelMocks
+			option.StateVersions = &v.StateVersions
+			option.Variables = &v.Variables
+			option.WorkspaceLocking = &v.WorkspaceLocking
+		}
+
+		_, err := w.tfClient.Client.TeamAccess.Add(ctx, option)
+		if err != nil {
+			w.log.Error(err, "Reconcile Team Access", "msg", "failed to create a new team access")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *WorkspaceReconciler) deleteTeamAccess(ctx context.Context, w *workspaceInstance, deleteTeamAccess map[string]*tfc.TeamAccess) error {
+	for _, v := range deleteTeamAccess {
+		err := w.tfClient.Client.TeamAccess.Remove(ctx, v.ID)
+		if err != nil {
+			w.log.Error(err, "Reconcile Team Access", "msg", "failed to delete team access")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *WorkspaceReconciler) updateTeamAccess(ctx context.Context, w *workspaceInstance, updateTeamAccess map[string]*tfc.TeamAccess) error {
+	for _, v := range updateTeamAccess {
+		w.log.Info("Reconcile Team Access", "msg", "updating team access")
+		option := tfc.TeamAccessUpdateOptions{
+			Access: &v.Access,
+		}
+
+		if v.Access == tfc.AccessCustom {
+			option.Runs = &v.Runs
+			option.RunTasks = &v.RunTasks
+			option.SentinelMocks = &v.SentinelMocks
+			option.StateVersions = &v.StateVersions
+			option.Variables = &v.Variables
+			option.WorkspaceLocking = &v.WorkspaceLocking
+		}
+
+		_, err := w.tfClient.Client.TeamAccess.Update(ctx, v.ID, option)
+		if err != nil {
+			w.log.Error(err, "Reconcile Team Access", "msg", "failed to update team access")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *WorkspaceReconciler) reconcileTeamAccess(ctx context.Context, w *workspaceInstance) error {
+	w.log.Info("Reconcile Team Access", "msg", "new reconciliation event")
+
+	specTeamAccess, err := r.getInstanceTeamAccess(ctx, w)
+	if err != nil {
+		w.log.Error(err, "Reconcile Team Access", "msg", "failed to get instance team access")
+		return err
+	}
+
+	workspaceTeamAccess, err := r.getWorkspaceTeamAccess(ctx, w)
+	if err != nil {
+		w.log.Error(err, "Reconcile Team Access", "msg", "failed to get workspace team access")
+		return err
+	}
+
+	createTeamAccess := getTeamAccessToCreate(specTeamAccess, workspaceTeamAccess)
+	if len(createTeamAccess) > 0 {
+		w.log.Info("Reconcile Team Access", "msg", fmt.Sprintf("creating %d team accesses", len(createTeamAccess)))
+		err := r.createTeamAccess(ctx, w, createTeamAccess)
+		if err != nil {
+			return err
+		}
+	}
+
+	updateTeamAccess := getTeamAccessToUpdate(specTeamAccess, workspaceTeamAccess)
+	if len(updateTeamAccess) > 0 {
+		w.log.Info("Reconcile Team Access", "msg", fmt.Sprintf("updating %d team accesses", len(updateTeamAccess)))
+		err := r.updateTeamAccess(ctx, w, updateTeamAccess)
+		if err != nil {
+			return err
+		}
+	}
+
+	deleteTeamAccess := getTeamAccessToDelete(specTeamAccess, workspaceTeamAccess)
+	if len(deleteTeamAccess) > 0 {
+		w.log.Info("Reconcile Team Access", "msg", fmt.Sprintf("deleting %d team accesses", len(deleteTeamAccess)))
+		err := r.deleteTeamAccess(ctx, w, deleteTeamAccess)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
