8cd0bc93c9e2a0e5d6c756f47aa409cc20d29f8e
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2024-11-18 16:25:52 +0100

    Update tests

---
+++ charts/unit/helpers.go
@@ -0,0 +1,44 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	helmChartName    = "hcp-terraform-operator"
+	helmChartVersion = "2.7.0"
+	helmChartPath    = "../hcp-terraform-operator"
+	helmReleaseName  = "this"
+
+	defaultNamespace = "default"
+
+	serviceAccountTemplate = "templates/serviceaccount.yaml"
+)
+
+var (
+	defaultServiceAccountName   = fmt.Sprintf("%s-%s", helmReleaseName, helmChartName)
+	defaultServiceAccountLabels = map[string]string{
+		"helm.sh/chart":                fmt.Sprintf("%s-%s", helmChartName, helmChartVersion),
+		"app.kubernetes.io/name":       helmChartName,
+		"app.kubernetes.io/instance":   helmReleaseName,
+		"app.kubernetes.io/version":    helmChartVersion,
+		"app.kubernetes.io/managed-by": "Helm",
+	}
+)
+
+func renderServiceAccountManifest(t *testing.T, options *helm.Options) corev1.ServiceAccount {
+	output, err := helm.RenderTemplateE(t, options, helmChartPath, helmReleaseName, []string{serviceAccountTemplate})
+	assert.NoError(t, err)
+
+	var sa corev1.ServiceAccount
+	helm.UnmarshalK8SYaml(t, output, &sa)
+	return sa
+}
