145dfb2bbcd81d1d825b8359ec5575b8cb4d9c79
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2024-11-25 19:39:48 +0100

    WIP

---
+++ internal/controller/workspace_controller_unit_test.go
@@ -0,0 +1,70 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package controller
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	appv1alpha2 "github.com/hashicorp/hcp-terraform-operator/api/v1alpha2"
+)
+
+var _ = Describe("Workspace controller", Ordered, func() {
+	var (
+		instance       *appv1alpha2.Workspace
+		namespacedName types.NamespacedName
+		workspace      string
+	)
+
+	BeforeAll(func() {
+		SetDefaultEventuallyTimeout(syncPeriod)
+	})
+
+	BeforeEach(func() {
+		namespacedName = newNamespacedName()
+		workspace = fmt.Sprintf("kubernetes-operator-%v", randomNumber())
+		// Create a new workspace object for each test
+		instance = &appv1alpha2.Workspace{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "app.terraform.io/v1alpha2",
+				Kind:       "Workspace",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              namespacedName.Name,
+				Namespace:         namespacedName.Namespace,
+				DeletionTimestamp: nil,
+				Finalizers:        []string{},
+			},
+			Spec: appv1alpha2.WorkspaceSpec{
+				Organization: organization,
+				Token: appv1alpha2.Token{
+					SecretKeyRef: &corev1.SecretKeySelector{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: secretNamespacedName.Name,
+						},
+						Key: secretKey,
+					},
+				},
+				Name: workspace,
+			},
+			Status: appv1alpha2.WorkspaceStatus{},
+		}
+	})
+
+	Context("Unit", func() {
+		It("Invalid Project ID", func() {
+			instance.Spec.Project = &appv1alpha2.WorkspaceProject{
+				ID: "this",
+			}
+			Expect(k8sClient.Create(ctx, instance, client.DryRunAll)).ShouldNot(Succeed())
+		})
+	})
+})
