61a96424d0467e54f033a247754a834aa19b582b
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2025-02-24 14:27:43 +0100

    Add tests

--- internal/controller/helpers_test.go
+++ internal/controller/helpers_test.go
@@ -163,6 +163,43 @@ var _ = Describe("Helpers", Label("Unit"), func() {
 		})
 	})
 
+	Context("MatchWildcardName", func() {
+		// True
+		It("match prefix", func() {
+			result := matchWildcardName("*-terraform-workspace", "hcp-terraform-workspace")
+			Expect(result).To(BeTrue())
+		})
+		It("match suffix", func() {
+			result := matchWildcardName("hcp-terraform-*", "hcp-terraform-workspace")
+			Expect(result).To(BeTrue())
+		})
+		It("match prefix and suffix", func() {
+			result := matchWildcardName("*-terraform-*", "hcp-terraform-workspace")
+			Expect(result).To(BeTrue())
+		})
+		It("match no prefix and no suffix", func() {
+			result := matchWildcardName("hcp-terraform-workspace", "hcp-terraform-workspace")
+			Expect(result).To(BeTrue())
+		})
+		// False
+		It("does not match prefix", func() {
+			result := matchWildcardName("*-terraform-workspace", "hcp-tf-workspace")
+			Expect(result).To(BeFalse())
+		})
+		It("does not match suffix", func() {
+			result := matchWildcardName("hcp-terraform-*", "hashicorp-tf-workspace")
+			Expect(result).To(BeFalse())
+		})
+		It("does not match prefix and suffix", func() {
+			result := matchWildcardName("*-terraform-*", "hcp-tf-workspace")
+			Expect(result).To(BeFalse())
+		})
+		It("does not match no prefix and no suffix", func() {
+			result := matchWildcardName("hcp-terraform-workspace", "hcp-tf-workspace")
+			Expect(result).To(BeFalse())
+		})
+	})
+
 	Context("ParseTFEVersion", func() {
 		It("Valid TFE version", func() {
 			version := "v202502-1"
