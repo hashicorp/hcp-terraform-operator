3b90118d0a271d3e8c92a9fc07d9c9899bb9df1f
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2025-03-20 10:05:06 +0100

    Update releasing guide (#579)

---
+++ hack/add-bundle-replaces.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# Ensure that 'yq' is installed in the bin directory by running 'make yq'
+YQ="$ROOT/bin/yq"
+CSV_FILE="bundle/manifests/hcp-terraform-operator.clusterserviceversion.yaml"
+
+echo "Set 'spec.replaces' field."
+PREV_VERSION=`git describe --tags --abbrev=0 $(git rev-list --tags --skip=1 --max-count=1)`
+echo "Previous version: $PREV_VERSION"
+
+yq -i ".spec.replaces = \"hcp-terraform-operator.$PREV_VERSION\"" $CSV_FILE
+yq -i "sort_keys(..)" $CSV_FILE
