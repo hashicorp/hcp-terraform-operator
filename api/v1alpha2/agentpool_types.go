e83ec7ae3d0cf7c7c49c9248105fa5fadc5b04b8
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2025-03-25 14:01:43 +0100

    Fix spelling

--- api/v1alpha2/agentpool_types.go
+++ api/v1alpha2/agentpool_types.go
@@ -11,7 +11,8 @@ import (
 // DeletionPolicy defines the strategy the Kubernetes operator uses when you delete a resource, either manually or by a system event.
 //
 // You must use one of the following values:
-// - `retain`: When you delete the custom resource, the operator does not delete the workspace.
+// - `retain`: When you delete the custom resource, the operator does not delete the agent pool.
+// - `destroy`: The operator will attempt to remove the managed HCP Terraform agent pool.
 type AgentPoolDeletionPolicy string
 
 const (
