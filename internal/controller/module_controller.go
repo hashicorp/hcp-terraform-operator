1574bb3f1ec4d2d466f425f18ebd62f0677ecf33
Shweta-Deshpande <49728296+Shweta-Deshpande@users.noreply.github.com>
2026-04-29 14:36:18 -0700

    🐛 Add configuration version when creating a new run for Module Conroller (#711)
    * Add configuration version when creating a new run for Module Conroller
    
    * Changelog

--- internal/controller/module_controller.go
+++ internal/controller/module_controller.go
@@ -403,8 +403,9 @@ func (r *ModuleReconciler) reconcileModule(ctx context.Context, m *moduleInstanc
 	if needNewRun(&m.instance) {
 		m.log.Info("Reconcile Run", "msg", "create a new run")
 		run, err := m.tfClient.Client.Runs.Create(ctx, tfc.RunCreateOptions{
-			Message:   tfc.String(runMessage),
-			Workspace: workspace,
+			Message:              tfc.String(runMessage),
+			Workspace:            workspace,
+			ConfigurationVersion: &tfc.ConfigurationVersion{ID: m.instance.Status.ConfigurationVersion.ID},
 		})
 		if err != nil {
 			m.log.Error(err, "Reconcile Run", "msg", "failed to create a new run")
